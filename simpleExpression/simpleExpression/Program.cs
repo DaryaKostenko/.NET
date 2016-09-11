6b0cee59df50e2480992754efcc0aa6b03b8a086
DaryaKostenko <dolce_vita_forever@mail.ru>
2016-09-11 17:28:36 +0300

    Add files via upload

---
+++ simpleExpression/simpleExpression/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace simpleExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = int.Parse(Console.ReadLine());
+            int s2 = int.Parse(Console.ReadLine());
+            int s3 = int.Parse(Console.ReadLine());
+            if((s2!=0)&&(s2!=1))
+                Console.WriteLine(s1-s2*s3);
+            else Console.WriteLine(s1-s2-s3);
+        }
+    }
+}
