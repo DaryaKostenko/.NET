0d7df94d8b99f15e310fa73db324dc438591b81b
DaryaKostenko <dolce_vita_forever@mail.ru>
2016-09-11 17:28:02 +0300

    Add files via upload

---
+++ Penguin/Penguin/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Penguin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            string s;
+            int[] count = new int[3];
+            for (int i = 0; i < int.Parse(n); i++)
+            {
+                s = Console.ReadLine();
+                if (s == "Emperor Penguin")
+                    count[0]++;
+                if (s == "Little Penguin")
+                    count[1]++;
+                if (s == "Macaroni Penguin")
+                    count[2]++;
+            }
+            
+            if (count[0] == count.Max())
+                Console.WriteLine("Emperor Penguin");
+            if (count[1] == count.Max())
+                Console.WriteLine("Little Penguin");
+            if (count[2] == count.Max())
+                Console.WriteLine("Macaroni Penguin");
+
+        }
+
+    }
+}
+
