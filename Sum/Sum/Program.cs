794b904e8d03caa319404de9c4d4f8e06b2f239c
DaryaKostenko <dolce_vita_forever@mail.ru>
2016-09-11 17:29:08 +0300

    Add files via upload

---
+++ Sum/Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int answer=0;
+            if (n>0)
+                for (int i = 1; i <= n; i++)
+                    answer+=i;
+            else
+                for (int i = n; i <= 1; i++)
+                    answer += i;
+            Console.WriteLine(answer);
+        }
+    }
+}
